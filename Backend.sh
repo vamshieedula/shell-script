bfb957dbf9849565e2bb12395918ff6169a7b636
vamshi <vamshi1608@gmail.com>
2025-03-03 11:04:14 +0530

    shell scripting

--- Backend.sh
+++ Backend.sh
@@ -7,8 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo “please enter DB password”
-read -s mysql_root_password”
+echo "please enter DB password"
+read -s "mysql_root_password"
 
 Validate(){
 	if [ $1 -ne 0 ]
